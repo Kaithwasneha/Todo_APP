d7f0412572718b212410cd413e06d42df07e6126
Neha <Kaithwasneha720@gmail.com>
2024-12-28 00:11:37 +0530

    todo-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+import Footer from './Footer'
+
+function App() {
+  return (
+    <main>
+      <Home/>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <div className="footer">
+     <br /><br />
+     <h1>ToDo Application Done By Neha kaithwas</h1>
+     </div>
+    </>
+  )
+}
+
+export default Footer
